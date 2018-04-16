3fac6273a8ee580e44a9dc4f419d59208b6c5cf8
KATOH Yasufumi <karma@jazz.email.ne.jp>
2018-04-16 21:00:12 +0900

    やっぱり /var/log/package 内のファイルから情報を取得するようにした

--- get_plamobuild.rb
+++ get_plamobuild.rb
@@ -1,19 +1,31 @@
 #!/usr/bin/ruby
+# coding: utf-8
 require 'systemu'
 
 PACKAGEINFO_DIR = "/var/log/packages"
 DOCDIR = "/usr/share/doc"
 REPO_DIR = "./Plamo-src"
+FAIL_FILE = "./failure.txt"
+LOCATION = "/var/adm/mount/plamo/"
 
-def get_packages()
-  packages = Array.new
-  Dir.open(PACKAGEINFO_DIR).each do |package|
-    if package == "." || package == ".." then
-      next
+def get_package_infos()
+  Dir.glob("#{PACKAGEINFO_DIR}/*") do |f|
+    package_name = f.delete_prefix("#{PACKAGEINFO_DIR}/")
+    io = open(f, "r")
+    while line = io.gets
+      if /PACKAGE NAME:\s+(.*?)-(.*?)-(.*?)-(.*)/ =~ line then
+        version = $2
+      end
+      if %r{PACKAGE LOCATION:\s+(.+)/([0-9a-z_]+?)/} =~ line then
+        category = $2
+      end
+      if /PlamoBuild/ =~ line then
+        script = "/#{line}"
+      end
     end
-    packages.push(package)
+    puts "plamo/#{category}/#{package_name}/"
+    io.close
   end
-  return packages
 end
 
 def get_category(package)
@@ -30,12 +42,25 @@ def get_category(package)
 end
 
 def copy_buildscript()
-  Dir.glob("#{DOCDIR}/*/PlamoBuild.*.gz").each do |script|
-    puts script
+  # docdir の PlamoBuild.* をサーチ
+  Dir.glob("#{DOCDIR}/*/PlamoBuild.*.gz").each do |fullpath|
+    if /(.*)\/(PlamoBuild.*)/ =~ fullpath.delete_prefix("#{DOCDIR}/") then
+      script_gz = $2
+      script = script_gz.delete_suffix(".gz")
+      if /(.*)-([0-9.-_]+)/ =~ script then
+        package = $1.delete_prefix("PlamoBuild.").tr("-", "_")
+        version = $2
+      end
+      if !FileTest.exist?("#{PACKAGEINFO_DIR}/#{package}") then
+        io = open(FAIL_FILE, "a")
+        io.seek(0, IO::SEEK_END)
+        io.puts("Script: #{script}\t\tPackage: #{package}\t\tVersion: #{version}")
+        io.close
+      end
+      puts "Script: #{script}\t\tPackage: #{package}\t\tVersion: #{version}"
+    end
   end
 end
 
-#p get_packages
-#p get_src("lxc")
-#p $packages
-copy_buildscript
+#copy_buildscript
+get_package_infos
