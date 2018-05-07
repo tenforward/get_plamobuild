f968c3de3374034159131a0f240c675a00eb9640
KATOH Yasufumi <karma@jazz.email.ne.jp>
2018-05-07 21:20:59 +0900

    decompress .gz files

--- get_plamobuild.rb
+++ get_plamobuild.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/ruby
 # coding: utf-8
+
 require 'systemu'
 
 PACKAGEINFO_DIR = "/var/log/packages"
 DOCDIR = "/usr/share/doc"
-REPO_DIR = "./Plamo-src"
+REPO_DIR = "../Plamo-src/plamo"
 FAIL_FILE = "./failure.txt"
 LOCATION = "/var/adm/mount/plamo/"
+PKGLISTFILE = "./package_list"
 
 $related = ["patch", "init", "conf", "service", ".cf"]
 
@@ -28,12 +30,19 @@ def get_package_infos()
         script_dir = File.dirname(script_path)
       end
     end
+    
     if category.nil? || category.empty? then
-      log = open("failure.txt", "a")
-      log.seek(0, IO::SEEK_END)
-      log.puts("#{package_name}: no category")
-      log.close
-    elsif script_path.nil? || script_path.empty? then
+      category = fallback_category(package_name)
+
+      if !category then
+        log = open("failure.txt", "a")
+        log.seek(0, IO::SEEK_END)
+        log.puts("#{package_name}: failure to get category")
+        log.close
+      end
+    end
+    
+    if script_path.nil? || script_path.empty? then
       log = open("failure.txt", "a")
       log.seek(0, IO::SEEK_END)
       log.puts("#{package_name}: No PlamoBuild")
@@ -42,8 +51,15 @@ def get_package_infos()
       to_dir = "#{REPO_DIR}/#{category}/#{package_name}"
       FileUtils.mkdir_p(to_dir, {:verbose => false})
       FileUtils.copy(script_path, to_dir, {:verbose => false})
+      if File.extname(script_path) == ".gz" then
+        file = File.basename(script_path)
+        system("gzip -df #{to_dir}/#{file}")
+      end
       get_related_files(script_dir).each do |file|
         FileUtils.copy("#{script_dir}/#{file}", to_dir, {:verbose => true})
+        if File.extname(file) == ".gz" then
+          system("gzip -df #{to_dir}/#{file}")
+        end
       end
     end
     io.close
@@ -68,5 +84,18 @@ def get_related_files(dir)
   return files
 end
 
+def fallback_category(pkgname)
+  open(PKGLISTFILE, "r").each do |line|
+    if /#{pkgname}.*\.txz/ =~ line
+      line.chomp!
+      if %r{/([0-9]+_[a-z0-9]+?)/} =~ line then
+        return $1
+      end
+    end
+  end
+  return false
+end
+
 get_package_infos
 #get_related_files("/usr/share/doc/dhcp-4.4.1/")
+#puts fallback_category("font_xfree86_type1")
